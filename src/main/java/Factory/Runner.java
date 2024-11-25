b5a88f3002d78441d9a0e211888409e92806422e
sssongbird <caldur33@gmail.com>
2024-11-25 11:25:10 +0100

    Implementierung Abfrage Wunsch Tabelle

--- src/main/java/Factory/Runner.java
+++ src/main/java/Factory/Runner.java
@@ -5,6 +5,7 @@ import FactoryLager.LagerDaoImplement;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
@@ -29,8 +30,62 @@ public class Runner {
 
             }
         };
-        TablePrinter.printTable("Lagerbestand House of Sounds", lagerDao.getAll());
+        Scanner scanner = new Scanner(System.in);
+        boolean continueProgram = true;
+
+
+
+        while (continueProgram) {
+            // Menü anzeigen
+            System.out.println("\n=== House of Sounds Datenbankabfrage ===");
+            System.out.println("Bitte wählen Sie eine Tabelle zur Anzeige:");
+            System.out.println("1. Lagerbestand");
+            System.out.println("2. Produkte");
+            System.out.println("3. Kunden");
+            System.out.println("0. Programm beenden");
+            System.out.print("\nIhre Wahl: ");
+
+            // Benutzereingabe einlesen
+            String choice = scanner.nextLine().trim();
+
+            // Eingabe verarbeiten
+            switch (choice) {
+                case "1":
+                    System.out.println("\nLade Lagerbestand...");
+                    TablePrinter.printTable("Lagerbestand House of Sounds", lagerDao.getAll());
+                    break;
+
+                case "2":
+                    System.out.println("\nFunktion noch nicht implementiert.");
+                    // Sobald ProduktDao implementiert ist:
+                    // TablePrinter.printTable("Produkte House of Sounds", produktDao.getAll());
+                    break;
+
+                case "3":
+                    System.out.println("\nFunktion noch nicht implementiert.");
+                    // Sobald KundeDao implementiert ist:
+                    // TablePrinter.printTable("Kunden House of Sounds", kundeDao.getAll());
+                    break;
+
+                case "0":
+                    continueProgram = false;
+                    System.out.println("\nProgramm wird beendet. Auf Wiedersehen!");
+                    break;
+
+                default:
+                    System.out.println("\nUngültige Eingabe! Bitte wählen Sie eine Zahl zwischen 0 und 3.");
+                    break;
+            }
+
+            if (continueProgram) {
+                System.out.println("\nDrücken Sie ENTER, um fortzufahren...");
+                scanner.nextLine();
+            }
+        }
+
+        scanner.close();
+    }
 
 
     }
-}
+
